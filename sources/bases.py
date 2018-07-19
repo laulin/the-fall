49a1e4f524c4d14453e0ec0ed12e309ed1c8d79c
laulin <laulin@nope.fr>
2018-07-19 21:45:09 +0200

    - Fix naming problem on Base namedtuple - Add get_level function

--- sources/bases.py
+++ sources/bases.py
@@ -5,7 +5,7 @@ from itertools import cycle
 # this module manages the value's base of each player. The main key is the 
 # player_id.
 
-Base = namedtuple("Base", [ "level_trade_post", 
+Base = namedtuple("Base", [ "level_trading_post", 
                             "level_weapon_factory", 
                             "level_vehicule_warhouse", 
                             "level_barack"])
@@ -26,6 +26,16 @@ class Bases:
         base = Base(*self._INIT_BASE_VALUES)
         self._base_dict[player_id] = base
 
+    def get_level(self, player_id, building):
+        # this function is used to get level of one building
+        base = self._base_dict[player_id]
+        field = "level_{}".format(building)
+
+        if field not in base._fields:
+            raise Exception("building {} doesn't exists".format(building))
+
+        return base._asdict()[field]
+
 class TestBases(unittest.TestCase):
     def test_init(self):
         bases = Bases()
@@ -42,6 +52,23 @@ class TestBases(unittest.TestCase):
         # error !
         with self.assertRaises(Exception):
             bases.create(player_id)
+
+    def test_get_level(self):
+        bases = Bases()
+        player_id = 10
+        bases.create(player_id)
+
+        result = bases.get_level(player_id, "trading_post")
+
+        self.assertEqual(result, 0)
+
+    def test_get_level_no_player(self):
+        bases = Bases()
+        player_id = 10
+        bases.create(player_id)
+
+        with self.assertRaises(Exception):
+            bases.get_level(player_id-1, "trading_post")
 
 
 if __name__ == "__main__":
